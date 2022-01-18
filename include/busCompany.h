282c8a6e61e611f48e1defb78fa38071a2507201
Nuno Pereira <nunoafonso2002@gmail.com>
2022-01-18 14:28:43 +0000

    Started working on project

---
+++ include/busCompany.h
@@ -0,0 +1,14 @@
+//
+// Created by naapperas on 18/01/22.
+//
+
+#ifndef PROJECT2_BUSCOMPANY_H
+#define PROJECT2_BUSCOMPANY_H
+
+
+class STCP {
+
+};
+
+
+#endif //PROJECT2_BUSCOMPANY_H
---
+++ src/busCompany.cpp
@@ -0,0 +1,5 @@
+//
+// Created by naapperas on 18/01/22.
+//
+
+#include "../include/busCompany.h"
