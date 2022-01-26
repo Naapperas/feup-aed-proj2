4c8af383dbc26af767323e7119079aa821ec2bcb
pedronunes19 <pedromiguelmnunes@gmail.com>
2022-01-26 17:18:15 +0000

    store line codes in a set

--- src/graph.cpp
+++ src/graph.cpp
@@ -24,8 +24,7 @@ void Graph::addEdge(const std::string& oStop, const std::string& dStop, const st
     auto existingEdge = std::find_if(oItr->second.adj.begin(), oItr->second.adj.end(), [dStop](const Edge &e){return e.dest==dStop;});
 
     if (existingEdge != oItr->second.adj.end()) {
-        if (std::find_if(existingEdge->lineCodes.begin(), existingEdge->lineCodes.end(), [lineCode](const std::string& s){return s == lineCode;}) == existingEdge->lineCodes.end())
-            existingEdge->lineCodes.push_back(lineCode);
+        existingEdge->lineCodes.insert(lineCode);
     } else {
         oItr->second.adj.push_back({dStop, distance, {lineCode}});
         if (!this->hasDir)
